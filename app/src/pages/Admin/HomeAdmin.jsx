95cbd59a7a7c11c7770611d4007d8fe75bd46164
giovannyjvr <giovannyjvr@al.insper.edu.br>
2024-05-28 15:52:56 -0300

    começando admin

--- app/src/pages/Admin/HomeAdmin.jsx
+++ app/src/pages/Admin/HomeAdmin.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import './staticAdmin/HomeAdmin.css';
+import Header from '../header';
+
 
 function HomeAdmin() {
     return (
         <div>
-            <h1>Hello, admin!</h1>
+            <Header />
+            
+            <div className="search-container">
+                <h1>Bem vindo ADMIN </h1>
+
+                
+            </div>
         </div>
     );
 }
-
 export default HomeAdmin;
