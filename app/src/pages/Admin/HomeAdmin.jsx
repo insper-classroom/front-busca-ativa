403d4abd955bb2d691fc933152ceec4fb3f505a3
giovannyjvr <giovannyjvr@al.insper.edu.br>
2024-05-29 10:46:43 -0300

    home bem encaminhada falta alguns filtros, botão de filtro e css

--- app/src/pages/Admin/HomeAdmin.jsx
+++ app/src/pages/Admin/HomeAdmin.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
+import HeaderAdmin from './HeaderAdmin';
+import CasosTable from './CasosTable'; // Importando o componente CasosTable
 import './staticAdmin/HomeAdmin.css';
-import Header from '../header';
-
 
 function HomeAdmin() {
     return (
-        <div>
-            <Header />
-            
+        <div className="home-admin">
+            <HeaderAdmin />
             <div className="search-container">
-                <h1>Bem vindo ADMIN </h1>
-
-                
+                <h1>Bem vindo ADMIN</h1>
+                <CasosTable /> {/* Utilizando o componente CasosTable */}
             </div>
         </div>
     );
 }
+
 export default HomeAdmin;
