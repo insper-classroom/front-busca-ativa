10c45398be83f21848308bc93061600e5c4f7d98
juliaas2 <juliaas@al.insper.edu.br>
2024-06-04 11:09:47 -0300

    pagina alunos dados editar

--- app/src/pages/Admin/DadosAlunos.jsx
+++ app/src/pages/Admin/DadosAlunos.jsx
@@ -2,10 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import Cookies from 'universal-cookie';
 import HeaderAdmin from './HeaderAdmin';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import Paper from '@mui/material/Paper';
+import { Grid, TextField, Button, Paper, Box, Typography, Container } from '@mui/material';
+import './static/Cadastro.css';
 
 const cookies = new Cookies();
 
@@ -73,111 +71,152 @@ function DadosAlunos() {
   };
 
   return (
-    <div className='user-control'>
+    <div>
       <HeaderAdmin />
-      <Paper sx={{ padding: '16px', margin: '16px' }}>
-        {aluno ? (
-          <div>
-            <h2>Detalhes do Aluno</h2>
-            <Box
-              component="form"
-              sx={{
-                '& > :not(style)': { m: 1, width: '25ch' },
-              }}
-              noValidate
-              autoComplete="off"
-            >
-            <TextField
-            name="nome"
-            label="Nome"
-            variant="filled"
-            value={editedAluno.nome || ''}
-            onChange={handleInputChange}
-            InputProps={{
-                readOnly: !editMode,
-            }}
-            />
-            <TextField
-            name="turma"
-            label="Turma"
-            variant="filled"
-            value={editedAluno.turma || ''}
-            onChange={handleInputChange}
-            InputProps={{
-                readOnly: !editMode,
-            }}
-            />
-            <TextField
-            name="RA"
-            label="RA"
-            variant="filled"
-            value={editedAluno.RA || ''}
-            InputProps={{
-                readOnly: true,
-            }}
-            />    
-            <TextField
-            name="endereco"
-            label="Endereço"
-            variant="filled"
-            value={editedAluno.endereco || ''}
-            onChange={handleInputChange}
-            InputProps={{
-                readOnly: !editMode,
-            }}
-            />
-            <TextField
-            name="telefone"
-            label="Telefone"
-            variant="filled"
-            value={editedAluno.telefone || ''}
-            onChange={handleInputChange}
-            InputProps={{
-                readOnly: !editMode,
-            }}
-            />
-            <TextField
-            name="telefone2"
-            label="Telefone 2"
-            variant="filled"
-            value={editedAluno.telefone2 || ''}
-            onChange={handleInputChange}
-            InputProps={{
-                readOnly: !editMode,
-            }}
-            />
-            <TextField
-            name="responsavel"
-            label="Responsável"
-            variant="filled"
-            value={editedAluno.responsavel || ''}
-            onChange={handleInputChange}
-            InputProps={{
-                readOnly: !editMode,
-            }}
-            />
-            <TextField
-            name="responsavel2"
-            label="Responsável 2"
-            variant="filled"
-            value={editedAluno.responsavel2 || ''}
-            onChange={handleInputChange}
-            InputProps={{
-                readOnly: !editMode,
-            }}
-            />
-            </Box>
-            {editMode ? (
-              <Button variant="contained" onClick={handleSave}>Salvar</Button>
-            ) : (
-              <Button variant="contained" onClick={() => setEditMode(true)}>Editar</Button>
-            )}
-            <Button variant="contained" component={Link} to="/alunos">Voltar</Button>
-          </div>
-        ) : (
-          <div>Carregando...</div>
-        )}
-      </Paper>
+      <br />
+      <div className='geral'>
+        <Grid container spacing={2} className="login-container">
+          <Grid item xs={12} style={{ textAlign: 'center' }}>
+            <Container maxWidth="md">
+              <Paper sx={{ padding: '16px', margin: '16px' }}>
+                {aluno ? (
+                  <Box component="form" noValidate autoComplete="off">
+                    <br/>
+                    <Typography component="h1" variant="h5" className="form-title">
+                      Detalhes do Aluno
+                    </Typography>
+                    <br/>
+                    <Grid container spacing={2}>
+                      <Grid item xs={12} sm={6}>
+                        <TextField
+                          name="nome"
+                          label="Nome"
+                          variant="outlined"
+                          fullWidth
+                          value={editedAluno.nome || ''}
+                          onChange={handleInputChange}
+                          InputProps={{
+                            readOnly: !editMode,
+                            style: { backgroundColor: editMode ? 'white' : 'inherit' }
+                          }}
+                        />
+                      </Grid>
+                      <Grid item xs={12} sm={6}>
+                        <TextField
+                          name="turma"
+                          label="Turma"
+                          variant="outlined"
+                          fullWidth
+                          value={editedAluno.turma || ''}
+                          onChange={handleInputChange}
+                          InputProps={{
+                            readOnly: !editMode,
+                            style: { backgroundColor: editMode ? 'white' : 'inherit' }
+                          }}
+                        />
+                      </Grid>
+                      <Grid item xs={12} sm={6}>
+                        <TextField
+                          name="RA"
+                          label="RA"
+                          variant="outlined"
+                          fullWidth
+                          value={editedAluno.RA || ''}
+                          InputProps={{
+                            readOnly: true,
+                            style: { backgroundColor: 'inherit' }
+                          }}
+                        />
+                      </Grid>
+                      <Grid item xs={12} sm={6}>
+                        <TextField
+                          name="endereco"
+                          label="Endereço"
+                          variant="outlined"
+                          fullWidth
+                          value={editedAluno.endereco || ''}
+                          onChange={handleInputChange}
+                          InputProps={{
+                            readOnly: !editMode,
+                            style: { backgroundColor: editMode ? 'white' : 'inherit' }
+                          }}
+                        />
+                      </Grid>
+                      <Grid item xs={12} sm={6}>
+                        <TextField
+                          name="telefone"
+                          label="Telefone"
+                          variant="outlined"
+                          fullWidth
+                          value={editedAluno.telefone || ''}
+                          onChange={handleInputChange}
+                          InputProps={{
+                            readOnly: !editMode,
+                            style: { backgroundColor: editMode ? 'white' : 'inherit' }
+                          }}
+                        />
+                      </Grid>
+                      <Grid item xs={12} sm={6}>
+                        <TextField
+                          name="telefone2"
+                          label="Telefone 2"
+                          variant="outlined"
+                          fullWidth
+                          value={editedAluno.telefone2 || ''}
+                          onChange={handleInputChange}
+                          InputProps={{
+                            readOnly: !editMode,
+                            style: { backgroundColor: editMode ? 'white' : 'inherit' }
+                          }}
+                        />
+                      </Grid>
+                      <Grid item xs={12} sm={6}>
+                        <TextField
+                          name="responsavel"
+                          label="Responsável"
+                          variant="outlined"
+                          fullWidth
+                          value={editedAluno.responsavel || ''}
+                          onChange={handleInputChange}
+                          InputProps={{
+                            readOnly: !editMode,
+                            style: { backgroundColor: editMode ? 'white' : 'inherit' }
+                          }}
+                        />
+                      </Grid>
+                      <Grid item xs={12} sm={6}>
+                        <TextField
+                          name="responsavel2"
+                          label="Responsável 2"
+                          variant="outlined"
+                          fullWidth
+                          value={editedAluno.responsavel2 || ''}
+                          onChange={handleInputChange}
+                          InputProps={{
+                            readOnly: !editMode,
+                            style: { backgroundColor: editMode ? 'white' : 'inherit' }
+                          }}
+                        />
+                      </Grid>
+                    </Grid>
+                    <Box sx={{ marginTop: '16px', display: 'flex', justifyContent: 'space-between' }}>
+                      <Button variant="contained" component={Link} to="/alunos">Voltar</Button>
+                      {editMode ? (
+                        <Button variant="contained" onClick={handleSave}>Salvar</Button>
+                      ) : (
+                        <Button variant="contained" onClick={() => setEditMode(true)}>Editar</Button>
+                      )}
+                    </Box>
+                  </Box>
+                ) : (
+                  <div>Carregando...</div>
+                )}
+              </Paper>
+            </Container>
+          </Grid>
+        </Grid>
+      </div>
+      <br />
     </div>
   );
 }
